9c0cb438a0d93d3843aa34d6a34a8552e1f361a2
DemonNyan <nyanlindemon@gmail.com>
2026-02-25 21:26:00 +0630

    Rename Skills component to Skill

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
-import Skills from "./components/Skill";
+import Skill from "./components/Skill";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
@@ -16,7 +16,7 @@ const App = () => {
       <CustomCursor />
       <NavBar />
       <Hero />
-      <Skills />
+      <Skill/>
 
       <About />
       <Projects />
