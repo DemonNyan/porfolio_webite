a7009f2eb4e5909f9989be5a5fd14bf358725d8d
NyanLinHtet <nyanlindemon@gmail.com>
2026-02-26 12:02:00 +0630

    phone change

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -20,7 +20,7 @@ export default function PortfolioNavbar() {
   return (
     <Navbar
       fluid
-      className="fixed top-0 w-full z-50 border-b border-gray-100 bg-white/80 backdrop-blur-md px-4 py-3"
+      className="fixed top-0 text-blue-400 w-full z-50 border-b border-gray-100 bg-white/80 backdrop-blur-md px-4 py-3"
     >
       <NavbarBrand href="#home" onClick={() => setActiveTab("Home")}>
         <span className="self-center whitespace-nowrap text-2xl font-black tracking-tight ">
@@ -43,7 +43,7 @@ export default function PortfolioNavbar() {
             className={`text-base font-bold py-3 md:py-0 transition-all duration-300 
               ${
                 activeTab === link.name
-                  ? "text-blue-600 md:border-b-2 md:border-blue-600"
+                  ? "text-white md:border-b-2 md:border-blue-600"
                   : "text-gray-600 hover:text-blue-400"
               }`}
           >
